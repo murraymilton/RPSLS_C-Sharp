5e8d06dae7c01692a5c7c2bb710c149467df4e20
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-05 21:05:46 +0200

    Made corrections display class

--- RPSLS1/GameDisplay.cs
+++ RPSLS1/GameDisplay.cs
@@ -14,13 +14,9 @@ namespace RPSLS1
 
         public GameDisplay() 
         {
-            Console.WriteLine($"Welcome ot Rock, Paper, Scisoors, Lizard, Spock!");
-            Thread.Sleep(1000);
-            Console.WriteLine($"Rules are simply just like Rock, Paper, Scissors. Just with the addition of 2 more options of lizard and spock.");
+            Console.WriteLine($"Welcome Rock, Paper, Scisoors, Lizard, Spock!");
             Thread.Sleep(1000);
             Console.WriteLine($"Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock");
-            Thread.Sleep(1000);
-            Console.WriteLine($"Got it, good");
             Thread.Sleep(1000);
             Console.WriteLine($"For the number of players enter 1 to play against the AI, enter 2 to play against another human");
             Thread.Sleep(1000);
@@ -31,3 +27,20 @@ namespace RPSLS1
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
