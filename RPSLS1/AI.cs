f891e2e77519c327b41cf8511399b608afee9c54
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-05 20:42:37 +0200

    created methods and constuctors for classes

--- RPSLS1/AI.cs
+++ RPSLS1/AI.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace RPSLS1
 {
-    class AI
+    public class AI
     {
+        string[] AInames = { "Ashtosva Netsar ", "Jaderigo Cudgorb", "Breliz Cotbar", "Rubkari Horred", "Zacmis Furmey", "Airiam" };
+
+        //Create AI Player name
+        Random rand = new Random();
+        int index = Random.Next(AInames.Length);
+        public AI()
+        {
+            Console.WriteLine(this.name = $"{AInames[index]} "); 
+        }
+
+        public override Gesture SelectGesture()
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(gestureOptions.Length);
+            Gesture SelectedGesture = gestureOptions[randomIndex];
+            this.gesture = new Gesture(SelectedGesture.name, SelectedGesture.wins, SelectedGesture.chosenGesture);
+            return gesture;
+        }
     }
 }
--- RPSLS1/GameDisplay.cs
+++ RPSLS1/GameDisplay.cs
@@ -8,6 +8,26 @@ namespace RPSLS1
 {
     public class GameDisplay
     {
+        public Human playerOne = new Human();
+        public AI playerTwo = new AI();
+
+
+        public GameDisplay() 
+        {
+            Console.WriteLine($"Welcome ot Rock, Paper, Scisoors, Lizard, Spock!");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Rules are simply just like Rock, Paper, Scissors. Just with the addition of 2 more options of lizard and spock.");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Got it, good");
+            Thread.Sleep(1000);
+            Console.WriteLine($"For the number of players enter 1 to play against the AI, enter 2 to play against another human");
+            Thread.Sleep(1000);
+            Console.WriteLine($"when choosing a gesture hit a single number and hit enter the number that you have selected will not be displayed in terminal.");
+            Thread.Sleep(1000);
+
+        }
 
     }
 }
