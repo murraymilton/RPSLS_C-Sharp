a63ed2856c15e72a1c6b0140e5ac022a3d391139
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-05 18:31:54 +0200

    initial commit

---
+++ RPSLS1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myName = "Milton";
+            Console.WriteLine( myName);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
